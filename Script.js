1329ef0d6227434146086001f25d1b7f3377b839
michelschlaepfer-pixel <michelschlaepfer@gmail.com>
2025-11-20 10:33:37 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -54,3 +54,24 @@ setInterval(() => {
     currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
 }, 4000);
+// -----------------------------
+// ANIMATIONS AU SCROLL
+// -----------------------------
+const sections = document.querySelectorAll('section');
+
+function revealOnScroll() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if(sectionTop < triggerBottom){
+            section.classList.add('visible');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+
+// Exécuter une première fois au chargement
+revealOnScroll();
